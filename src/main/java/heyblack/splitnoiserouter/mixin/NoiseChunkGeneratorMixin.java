021b1c01c015799f8c2532bde375fb2a44a6d5c9
HeyBlack233 <88659388+HeyBlack233@users.noreply.github.com>
2023-04-07 00:42:28 +0800

    Add files via upload

--- src/main/java/heyblack/splitnoiserouter/mixin/NoiseChunkGeneratorMixin.java
+++ src/main/java/heyblack/splitnoiserouter/mixin/NoiseChunkGeneratorMixin.java
@@ -3,9 +3,10 @@ package heyblack.splitnoiserouter.mixin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.biome.source.util.VanillaTerrainParameters;
 import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
+import net.minecraft.world.gen.densityfunction.DensityFunctions;
+import net.minecraft.world.gen.noise.NoiseConfig;
 import net.minecraft.world.gen.noise.NoiseRouter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,18 +18,17 @@ import java.util.List;
 
 @Environment(EnvType.CLIENT)
 @Mixin(NoiseChunkGenerator.class)
-public class NoiseChunkGeneratorMixin
+public abstract class NoiseChunkGeneratorMixin
 {
-    private NoiseRouter noiseRouter;
-
     @Inject(method = "getDebugHudText", at = @At("HEAD"), cancellable = true)
-    public void showMeMyCrossHair(List<String> text, BlockPos pos, CallbackInfo ci)
+    public void showMeMyCrossHair(List<String> text, NoiseConfig noiseConfig, BlockPos pos, CallbackInfo ci)
     {
         DecimalFormat decimalFormat = new DecimalFormat("0.000");
+        NoiseRouter noiseRouter = noiseConfig.getNoiseRouter();
         DensityFunction.UnblendedNoisePos unblendedNoisePos = new DensityFunction.UnblendedNoisePos(pos.getX(), pos.getY(), pos.getZ());
-        double d = this.noiseRouter.ridges().sample(unblendedNoisePos);
-        text.add("NoiseRouter T: " + decimalFormat.format(this.noiseRouter.temperature().sample(unblendedNoisePos)) + " H: " + decimalFormat.format(this.noiseRouter.humidity().sample(unblendedNoisePos)) + " C: " + decimalFormat.format(this.noiseRouter.continents().sample(unblendedNoisePos)) + " E: " + decimalFormat.format(this.noiseRouter.erosion().sample(unblendedNoisePos)) + " D: " + decimalFormat.format(this.noiseRouter.depth().sample(unblendedNoisePos)));
-        text.add("NoiseRouter W: " + decimalFormat.format(d) + " PV: " + decimalFormat.format(VanillaTerrainParameters.getNormalizedWeirdness((float)d)) + " AS: " + decimalFormat.format(this.noiseRouter.initialDensityWithoutJaggedness().sample(unblendedNoisePos)) + " N: " + decimalFormat.format(this.noiseRouter.finalDensity().sample(unblendedNoisePos)));
+        double d = noiseRouter.ridges().sample(unblendedNoisePos);
+        text.add("NoiseRouter T: " + decimalFormat.format(noiseRouter.temperature().sample(unblendedNoisePos)) + " V: " + decimalFormat.format(noiseRouter.vegetation().sample(unblendedNoisePos)) + " C: " + decimalFormat.format(noiseRouter.continents().sample(unblendedNoisePos)) + " E: " + decimalFormat.format(noiseRouter.erosion().sample(unblendedNoisePos)) + " D: " + decimalFormat.format(noiseRouter.depth().sample(unblendedNoisePos)));
+        text.add("NoiseRouter W: " + decimalFormat.format(d) + " PV: " + decimalFormat.format(DensityFunctions.getPeaksValleysNoise((float)d)) + " AS: " + decimalFormat.format(noiseRouter.initialDensityWithoutJaggedness().sample(unblendedNoisePos)) + " N: " + decimalFormat.format(noiseRouter.finalDensity().sample(unblendedNoisePos)));
         ci.cancel();
     }
 }
